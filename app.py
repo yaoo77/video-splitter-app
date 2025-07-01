f45ad4032edc12c243ee6d1c42bd57ef0d80dacd
yaoo77 <aoioneforall77@gmail.com>
2025-07-01 11:28:33 +0900

    Add Heroku deployment configuration (Procfile, runtime.txt, port config)

--- app.py
+++ app.py
@@ -164,4 +164,5 @@ def download_zip(session_id):
 app_instance = app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
